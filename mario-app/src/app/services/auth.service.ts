a1b16edf69854b372d6a732d79cde293a35fec54
pablomariano <pablomariano@gmail.com>
2023-06-26 17:49:53 -0400

    Auth Service configurado

--- mario-app/src/app/services/auth.service.ts
+++ mario-app/src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { User } from '../shared/user.class'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public isLogged: any = false;
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) { 
+    afAuth.authState.subscribe( user => (this.isLogged = user));
+  }
+  //login
+  async onLogin (user: User){
+    try{
+      return await this.afAuth.signInWithEmailAndPassword(
+        user.email,
+        user.password
+      );
+    } catch (error){
+      console.log('Error en login', error);
+    }
+  }
+
+  //register
+  async onRegister (user: User){
+    try{
+      return await this.afAuth.createUserWithEmailAndPassword(
+        user.email,
+        user.password
+      )
+
+    } catch (error){
+      console.log('Error en registro', error);
+    }
+  }
 }
