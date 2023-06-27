1b9933c242642c3d6347da8cbc26325072c2cfef
pablomariano <pablomariano@gmail.com>
2023-06-26 20:49:00 -0400

    Registro y Login habilitados

--- mario-app/src/app/login/login.page.ts
+++ mario-app/src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { User } from '../shared/user.class';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  user : User = new User()
+  constructor(private router: Router, private authSvc:AuthService) { }
 
   ngOnInit() {
+  }
+
+  async onLogin(){
+    const user = await this.authSvc.onLogin(this.user);
+    
+    if (user) {
+      console.log('Sesión iniciada');
+      this.router.navigateByUrl('/');
+    }
   }
 
 }
