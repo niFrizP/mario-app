062a128d3b9b9e43ab53783bc2a997acac519beb
Иiкo FP <102751106+niFrizP@users.noreply.github.com>
2023-06-29 15:41:09 -0400

    reparación del ruteo

--- mario-app/src/app/home/home.page.ts
+++ mario-app/src/app/home/home.page.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
+  constructor(private authSvc: AuthService, private router: Router, private afAuth: AngularFireAuth) {}
 
-  ngOnInit() {
-  }
-
-  onLogout(){
-    console.log('Sesión cerrada')
-    this.afAuth.signOut();
-    this.router.navigateByUrl('/login');
-  }
   onLogout(){
     console.log('Sesión cerrada')
     this.afAuth.signOut();
--- mario-app/src/app/services/auth.service.ts
+++ mario-app/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth'
-import { User } from '../shared/user.class'
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { User } from '../shared/user.class'; 
 
 @Injectable({
   providedIn: 'root'
@@ -9,30 +9,31 @@ export class AuthService {
   public isLogged: any = false;
 
   constructor(public afAuth: AngularFireAuth) { 
-    afAuth.authState.subscribe( user => (this.isLogged = user));
+    afAuth.authState.subscribe(user => (this.isLogged = user));
   }
-  //login
-  async onLogin (user: User){
+
+  // login
+  async onLogin(user:User){
     try{
-      return await this.afAuth.signInWithEmailAndPassword(
-        user.email,
-        user.password
-      );
-    } catch (error){
-      console.log('Error en login', error);
+      return await this.afAuth.signInWithEmailAndPassword(user.email, user.password);
+
+    }catch(error){
+      console.log('Error en Login', error);
+      return undefined;
     }
   }
 
   //register
-  async onRegister (user: User){
+  async onRegister(user: User){
     try{
       return await this.afAuth.createUserWithEmailAndPassword(
         user.email,
         user.password
-      )
-
-    } catch (error){
+      );
+    }catch(error){
       console.log('Error en registro', error);
+      return undefined;
     }
   }
+
 }
