e89197a794c72b9b42190f937f7f7b0326e70188
pablomariano <pablomariano@gmail.com>
2023-06-28 23:52:29 -0400

    Home modificado

--- mario-app/src/app/home/home.page.ts
+++ mario-app/src/app/home/home.page.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private authSvc: AuthService, private router: Router, private afAuth: AngularFireAuth) {}
 
   onLogout(){
     console.log('Sesión cerrada')
