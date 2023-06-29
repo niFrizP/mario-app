7df531a979839fabf85dcc64c29780af9099041a
MatiasTorres0 <mat.torres@duocuc.cl>
2023-06-28 23:12:29 -0400

    modificar agregado, solo funciona de manera local

--- mario-app/src/app/app-routing.module.ts
+++ mario-app/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'modificar',
+    loadChildren: () => import('./modificar/modificar.module').then( m => m.ModificarPageModule)
   },
+
 ];
 
 @NgModule({
