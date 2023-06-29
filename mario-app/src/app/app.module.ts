450e300041c4d106d0ccf355a10053eb19655392
Иiкo FP <102751106+niFrizP@users.noreply.github.com>
2023-06-29 10:28:02 -0400

    gitignore y node_modules

--- mario-app/src/app/app.module.ts
+++ mario-app/src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-
-
-import { AngularFireModule } from '@angular/fire/compat'
-import { AngularFireAuthModule } from '@angular/fire/compat/auth'
-import { environment } from './../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
